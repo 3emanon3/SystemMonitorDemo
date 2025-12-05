f74949d29e2d73cbc9d4fa1a161887a6980994bf
3emanon3 <tanjuinwei1009@gmail.com>
2025-12-05 20:57:40 +0800

    Update assembly information and README with project details

--- MonitorApp/obj/Debug/net8.0-windows/MonitorApp.AssemblyInfo.cs
+++ MonitorApp/obj/Debug/net8.0-windows/MonitorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonitorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+308735128f0bdd0aa2fd4d6b0bd52e2f03836e06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f98aa41f62322e259d71aa8a831d234880bc36")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonitorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonitorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,4 @@
+A small demo of desktop application to monitor real-time CPU and Memory usage.
+Implemented MVVM architecture for clean separation of logic and UI using WPF (XAML).
+Utilized C# multi-threading (Task/Async) to update UI without freezing the main thread.
+Integrated a C++ DLL using P/Invoke to fetch low-level system uptime data.
